06990dea90ab47f8ad425cac746155dd4270d196
Travis Briggs <audiodude@gmail.com>
2017-04-05 20:13:53 -0700

    Remove favicon, and basic Google sign-in working.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+declare var gapi:any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  ngAfterViewInit() {
+    gapi.signin2.render('g-signin', {
+      'scope': 'profile email',
+      'width': 240,
+      'height': 50,
+      'onsuccess': this.onSignIn,
+    });
+  }
+
+  onSignIn = (googleUser: any) => {
+    window.console.log(googleUser);
+  }
 }
